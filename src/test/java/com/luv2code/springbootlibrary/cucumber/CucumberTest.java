b51b90d38bcf4e2ece4d1a7368ebe736871c04f9
Martin Baus <mb@dacomb.de>
2023-03-01 11:55:15 +0100

    Apply spring-boot-cucumber-mvc module

---
+++ src/test/java/com/luv2code/springbootlibrary/cucumber/CucumberTest.java
@@ -0,0 +1,17 @@
+package com.luv2code.springbootlibrary.cucumber;
+
+import com.luv2code.springbootlibrary.ComponentTest;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@ComponentTest
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  glue = "com.luv2code.springbootlibrary",
+  plugin = {
+    "pretty", "json:target/cucumber/cucumber.json", "html:target/cucumber/cucumber.htm", "junit:target/cucumber/TEST-cucumber.xml",
+  },
+  features = "src/test/features"
+)
+public class CucumberTest {}
---
+++ src/test/java/com/luv2code/springbootlibrary/cucumber/HeaderAsserter.java
@@ -0,0 +1,9 @@
+package com.luv2code.springbootlibrary.cucumber;
+
+public interface HeaderAsserter<T extends ResponseAsserter> {
+  HeaderAsserter<T> containing(String value);
+
+  HeaderAsserter<T> startingWith(String prefix);
+
+  T and();
+}
