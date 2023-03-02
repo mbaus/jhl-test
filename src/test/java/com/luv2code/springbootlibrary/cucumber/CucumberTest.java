2d88a0c6bc898e0b7b68f92fb4329d1a5e146ad9
Martin Baus <mb@dacomb.de>
2023-03-02 14:21:23 +0100

    ignore invoice Test. and add configuration to ignor Scenario un CucumberTest

--- src/test/java/com/luv2code/springbootlibrary/cucumber/CucumberTest.java
+++ src/test/java/com/luv2code/springbootlibrary/cucumber/CucumberTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @ComponentTest
 @RunWith(Cucumber.class)
 @CucumberOptions(
+  tags = "not @ignore",
   glue = "com.luv2code.springbootlibrary",
   plugin = {
     "pretty", "json:target/cucumber/cucumber.json", "html:target/cucumber/cucumber.htm", "junit:target/cucumber/TEST-cucumber.xml",
