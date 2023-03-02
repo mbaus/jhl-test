bfda52f099f62b0ee9c3fec3d8519313325bf3e0
Martin Baus <mb@dacomb.de>
2023-03-02 22:59:42 +0100

    Add Component Test Annotation

--- src/test/java/com/luv2code/springbootlibrary/invoice/infrastructure/primary/RestInvoiceToCreateTest.java
+++ src/test/java/com/luv2code/springbootlibrary/invoice/infrastructure/primary/RestInvoiceToCreateTest.java
@@ -3,11 +3,13 @@ package com.luv2code.springbootlibrary.invoice.infrastructure.primary;
 import static com.luv2code.springbootlibrary.invoice.domain.InvoicesFixture.invoiceToCreate;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.luv2code.springbootlibrary.ComponentTest;
 import com.luv2code.springbootlibrary.JsonHelper;
 import com.luv2code.springbootlibrary.invoice.domain.InvoicesFixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@ComponentTest
 class RestInvoiceToCreateTest {
 
   @Test
