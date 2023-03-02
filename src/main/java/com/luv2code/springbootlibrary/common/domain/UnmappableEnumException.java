746619648fb36226c8178e97911f29a22140c383
Martin Baus <mb@dacomb.de>
2023-03-02 22:27:23 +0100

    Apply java-enums module

---
+++ src/main/java/com/luv2code/springbootlibrary/common/domain/UnmappableEnumException.java
@@ -0,0 +1,8 @@
+package com.luv2code.springbootlibrary.common.domain;
+
+class UnmappableEnumException extends RuntimeException {
+
+  public <T extends Enum<T>, U extends Enum<U>> UnmappableEnumException(Class<T> from, Class<U> to) {
+    super("Can't map " + from + " to " + to);
+  }
+}
