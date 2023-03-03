e64d42682c44a45a9f464dafdf178bd33466d5cf
Martin Baus <mb@dacomb.de>
2023-03-03 15:47:15 +0100

    update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
   env: {
-    node: true,
+    browser: true,
+    es2021: true,
+    // node: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['react'],
   ignorePatterns: ['node_modules/'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-no-target-blank': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-no-target-blank': 0,
+    '@typescript-eslint/no-empty-function': 0,
   },
 };
