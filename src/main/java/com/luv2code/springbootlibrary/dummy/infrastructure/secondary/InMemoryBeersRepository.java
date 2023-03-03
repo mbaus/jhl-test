11f61a7a3d1357e1bdd21271cfdf95f416ce15df
Martin Baus <mb@dacomb.de>
2023-03-03 15:37:11 +0100

    make a empty java class

--- src/main/java/com/luv2code/springbootlibrary/dummy/infrastructure/secondary/InMemoryBeersRepository.java
+++ src/main/java/com/luv2code/springbootlibrary/dummy/infrastructure/secondary/InMemoryBeersRepository.java
@@ -1,4 +1,5 @@
-//package com.luv2code.springbootlibrary.dummy.infrastructure.secondary;
+package com.luv2code.springbootlibrary.dummy.infrastructure.secondary;
+
 //
 //import com.luv2code.springbootlibrary.dummy.domain.BeerId;
 //import com.luv2code.springbootlibrary.dummy.domain.beer.Beer;
@@ -13,34 +14,34 @@
 //import org.springframework.stereotype.Repository;
 //
 //@Repository
-//class InMemoryBeersRepository implements BeersRepository {
-//
-//  private final Map<BeerId, Beer> beers = new ConcurrentHashMap<>();
-//
-//  @Override
-//  public void save(Beer beer) {
-//    Assert.notNull("beer", beer);
-//
-//    beers.put(beer.id(), beer);
-//  }
-//
-//  @Override
-//  public Beers catalog() {
-//    return new Beers(beers.values().stream().filter(soldBeer()).toList());
-//  }
-//
-//  private Predicate<Beer> soldBeer() {
-//    return beer -> beer.sellingState() == BeerSellingState.SOLD;
-//  }
-//
-//  @Override
-//  public Optional<Beer> get(BeerId beer) {
-//    Assert.notNull("beer", beer);
-//
-//    return Optional.ofNullable(beers.get(beer));
-//  }
-//
-//  public void clear() {
-//    beers.clear();
-//  }
-//}
+class InMemoryBeersRepository { //implements BeersRepository
+  //
+  //  private final Map<BeerId, Beer> beers = new ConcurrentHashMap<>();
+  //
+  //  @Override
+  //  public void save(Beer beer) {
+  //    Assert.notNull("beer", beer);
+  //
+  //    beers.put(beer.id(), beer);
+  //  }
+  //
+  //  @Override
+  //  public Beers catalog() {
+  //    return new Beers(beers.values().stream().filter(soldBeer()).toList());
+  //  }
+  //
+  //  private Predicate<Beer> soldBeer() {
+  //    return beer -> beer.sellingState() == BeerSellingState.SOLD;
+  //  }
+  //
+  //  @Override
+  //  public Optional<Beer> get(BeerId beer) {
+  //    Assert.notNull("beer", beer);
+  //
+  //    return Optional.ofNullable(beers.get(beer));
+  //  }
+  //
+  //  public void clear() {
+  //    beers.clear();
+  //  }
+}
